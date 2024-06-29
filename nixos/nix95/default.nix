17d536e1cf92934b874da52414c70c4442ec1668
Enno Richter <enno@nerdworks.de>
2024-06-29 05:01:09 +0200

    implement nixos-24.05 module changes

--- nixos/nix95/default.nix
+++ nixos/nix95/default.nix
@@ -1,12 +1,19 @@
 { lib, pkgs, ... }: {
 
+  services.libinput = {
+    enable = lib.mkDefault true;
+    touchpad.naturalScrolling = true;
+    mouse.naturalScrolling = true;
+  };
+
+  services.displayManager.defaultSession = "xfce";
+
   services.xserver = {
     enable = true;
     desktopManager = {
       xterm.enable = false;
       xfce.enable = true;
     };
-    displayManager.defaultSession = "xfce";
     displayManager.lightdm = {
       background = "#008080";
       greeters.gtk = {
@@ -27,32 +34,37 @@
         '';
       };
     };
-    layout = lib.mkDefault "us";
-    libinput.enable = true;
-    libinput.touchpad.naturalScrolling = true;
-    libinput.mouse.naturalScrolling = true;
-    xkbOptions = "eurosign:e,terminate:ctrl_alt_bksp,compose:ralt";
+
+    xkb = {
+      layout = lib.mkDefault "us";
+      options = lib.mkDefault "eurosign:e,terminate:ctrl_alt_bksp,compose:ralt";
+    };
   };
+
   programs.thunar = {
-    enable = true;
+    enable = lib.mkDefault true;
     plugins = with pkgs.xfce; [
       thunar-archive-plugin
       thunar-media-tags-plugin
       thunar-volman
     ];
   };
+
   boot.plymouth = {
-    enable = true;
+    enable = lib.mkDefault true;
     logo = ../../assets/Microsoft_Windows_95_wordmark.png;
   };
-  environment.systemPackages = [
-    pkgs.chicago95
-    pkgs.libinput
-    pkgs.pavucontrol
-    pkgs.xclip
-    pkgs.xfce.xfce4-fsguard-plugin
-    pkgs.xfce.xfce4-pulseaudio-plugin
-    pkgs.xsel
+
+  environment.systemPackages = with pkgs; [
+    chicago95
+    libinput
+    pavucontrol
+    xarchiver
+    xclip
+    xfce.xfce4-fsguard-plugin
+    xfce.xfce4-pulseaudio-plugin
+    xsel
   ];
+
   fonts.packages = [ pkgs.chicago95 ];
 }
